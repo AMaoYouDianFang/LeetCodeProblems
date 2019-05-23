8c0eff1baf7b69f6510f1511f128982898b96547
liuxize <liuxize95@gmail.com>
2019-05-23 09:17:17 +0800

    5-23

--- Python/0144. Binary Tree Preorder Traversal/Main.py
+++ Python/0144. Binary Tree Preorder Traversal/Main.py
@@ -4,23 +4,35 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
-class Solution(object):
+class Solution:
     def preorderTraversal(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
         res = []
-        if not root :
-            return []
-        stack = [root]
-        while stack:
-            t = stack.pop()
-            res.append(t.val)
-            if t.right:
-                stack.append(t.right)
-            if t.left:
-                stack.append(t.left)
+        self.preor(root, res)
         return res
+    
+    def preor(self, root, res):
+        if not root: return 
+        res.append(root.val)
+        self.preor(root.left,res)
+        self.preor(root.right,res)
+
+    #迭代
+    def preorderTraversal(self, root):
+        res = []
+        tree = []
+        if not root : return res
+        tree.append(root)
+        while tree:
+            s = tree.pop()
+            res.append(s.val)
+            if s.right: 
+                tree.append(s.right)
+            if s.left:
+                tree.append(s.left)
+        return res
+        
+    
+    
+
 
      
