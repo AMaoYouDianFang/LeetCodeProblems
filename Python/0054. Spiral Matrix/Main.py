e6f489bf4cca5db44231d0a60a442771f2bf853d
liuxize <liuxize95@gmail.com>
2019-07-07 13:48:25 +0800

    7-7

--- Python/0054. Spiral Matrix/Main.py
+++ Python/0054. Spiral Matrix/Main.py
@@ -1,37 +1,40 @@
 class Solution:
     def spiralOrder(self, matrix):
-        res = []
-        n = len(matrix)
-
-        if n ==0:
+        if not matrix:
             return []
-        maxUp = maxLeft = 0
-        maxDown = len(matrix) - 1
-        maxRight = len(matrix[0]) - 1 
- 
-        direction = 0 # 0 go right, 1 go down, 2 go left, 3 up
-        while True:
-            if direction ==0:
-                for i in range(maxLeft, maxRight+1):
-                    res.append(matrix[maxUp][i])
-                maxUp +=1
-            elif direction == 1:
-                for i in range(maxUp, maxDown +1):
-                    res.append(matrix[i][maxRight])
-                maxRight -=1
-            elif direction == 2:
-                for i in reversed(range(maxLeft, maxRight+1)):
-                    res.append(matrix[maxDown][i])
-                maxDown -= 1
-            else:
-                for i in reversed(range(maxUp, maxDown+1)):
-                    res.append(matrix[i][maxLeft])
-                maxLeft+=1
-            if maxUp > maxDown or maxLeft > maxRight:  #结束条件
-                return res
-            direction = (direction +1)%4
+        top, left = 0, 0
+        right = len(matrix[0]) -1
+        bottom = len(matrix) -1
+        res = []
+        while (True):
+            for i in range(left, right+1):
+                res.append(matrix[top][i])
+                
+            top += 1
+            if top > bottom: break
+
+            for i in range(top, bottom+1):
+                res.append(matrix[i][right])
+            right -= 1
+            if left > right: break
+
+            for i in range(right, left-1, -1):
+                res.append(matrix[bottom][i])
+            bottom -=1
+            if bottom < top: break
+            
+            for i in range(bottom, top-1, -1):
+                res.append(matrix[i][left])
+                
+            left += 1
+            if left > right:break
+            
+        return res
+            
+
+
 
 if __name__ == "__main__":
     s = Solution()
-    res = s.spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+    res = s.spiralOrder([[2,5,8],[4,0,-1]])
     print(res)
